a7b5f2324a6d34ba5abbf1fe95caf9eb51498127
Simonini <descovi@gmail.com>
2012-09-29 12:15:58 +0200

    Caricamenti dell'audio.

--- menu_iniziale.lua
+++ menu_iniziale.lua
@@ -24,10 +24,7 @@ function menu_iniziale:createScene( event )
     group:insert(cerchio_container)
   end
 
-  function play_sound(event)
-    local url = event.target.audio_url
-    local path_audio = 'media/audio/vocali/'
-  end
+  
   
   function create_globulo( file_name , vocale)
     -- dati
@@ -49,45 +46,76 @@ function menu_iniziale:createScene( event )
     globulo.audio_url = 'caio'
     return globulo
   end
-  
+ 
+  -- Loading sound
+  local path_audio = 'media/audio/vocali/'
+  local a_l = audio.loadSound( path_audio .. 'A_L.mp3' )
+  local a_s = audio.loadSound( path_audio .. 'A_S.mp3' )
+  local e_l = audio.loadSound( path_audio .. 'E_L.mp3' )
+  local e_s = audio.loadSound( path_audio .. 'E_S.mp3' )
+  local i_l = audio.loadSound( path_audio .. 'I_L.mp3' )
+  local i_s = audio.loadSound( path_audio .. 'I_S.mp3' )
+  local o_l = audio.loadSound( path_audio .. 'O_L.mp3' )
+  local o_s = audio.loadSound( path_audio .. 'O_S.mp3' )
+  local u_l = audio.loadSound( path_audio .. 'U_L.mp3' )
+  local u_s = audio.loadSound( path_audio .. 'U_S.mp3' )
+
+  function play_sound(event)
+    print "pla"
+    local url = event.target.audio_url
+    audio.play( url ) 
+    print(url)
+  end
+
   -- LONG
   -- a
   a_long = create_globulo('long-a',vocali[1])
+  a_long.audio_url = a_l
   create_button_to_go(a_long, vocali[1])
   a_long:addEventListener('touch',play_sound)
   -- e
   e_long = create_globulo('long-e',vocali[2])
+  e_long.audio_url = e_l
   create_button_to_go(e_long, vocali[2])
   e_long:addEventListener('touch',play_sound)
   -- i
-  i_long = create_globulo('long-i',vocali[3])  
+  i_long = create_globulo('long-i',vocali[3]) 
+  i_long.audio_url = i_l
   create_button_to_go(i_long, vocali[3])
   i_long:addEventListener('touch',play_sound)
   -- o
   o_long = create_globulo('long-o',vocali[4])
+  o_long.audio_url = o_l
   create_button_to_go(o_long, vocali[4])
   o_long:addEventListener('touch',play_sound)
   -- u
   u_long = create_globulo('long-u',vocali[5])
+  u_long.audio_url = u_l
   create_button_to_go(u_long, vocali[5])
   u_long:addEventListener('touch',play_sound)
 
   -- SHORT
   -- a
   a_short = create_globulo('short-a',vocali[1])
+  a_short.audio_url = a_s
   a_short:addEventListener('touch',play_sound)
   -- e
   e_short = create_globulo('short-e',vocali[2])
+  e_short.audio_url = e_s
   e_short:addEventListener('touch',play_sound)
   -- i
   i_short = create_globulo('short-i',vocali[3])
+  i_short.audio_url = i_s
   i_short:addEventListener('touch',play_sound)
   -- o
   o_short = create_globulo('short-o',vocali[4])
+  o_short.audio_url = o_s
   o_short:addEventListener('touch',play_sound)
   -- u
   u_short = create_globulo('short-u',vocali[5])
+  u_short.audio_url = u_s
   u_short:addEventListener('touch',play_sound)
+  
 end
 
 --Add the createScene listener
