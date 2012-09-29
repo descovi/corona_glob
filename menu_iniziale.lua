2ebb84ef4aff725f137ab6f2d88255cf76a7699e
Simonini <descovi@gmail.com>
2012-09-29 11:36:01 +0200

    Menu iniziale. Mostra i pulsanti. Ancora solo vista. Manca ancora il funzionamento reale.

--- menu_iniziale.lua
+++ menu_iniziale.lua
@@ -8,9 +8,21 @@ function menu_iniziale:createScene( event )
   -- Creazioni globuli
   local counter = 1
   x_pos = {0, 100, 200, 300, 400, 0, 100, 200, 300, 400}
-  y_pos = {400, 600}
+  y_pos = {350, 550}
   globulo_size = 100
-  function create_globulo( file_name )
+  
+  local group = self.view
+  
+  function create_button_to_go(globulo, vocale)
+    local cerchio_container = display.newGroup()
+    local cerchio = display.newCircle(globulo.x, 700, 30)
+    local testo = display.newText(vocale, cerchio.x-6, cerchio.y-14, "Courier New", 18)
+    testo:setTextColor(0, 0, 0)
+    cerchio_container:insert(cerchio)
+    cerchio_container:insert(testo)
+    group:insert(cerchio_container)
+  end
+  function create_globulo( file_name , vocale)
     local path = 'media/menu_iniziale/'
     local end_path = '.png'
     local final_path = path .. file_name .. end_path
@@ -24,30 +36,32 @@ function menu_iniziale:createScene( event )
     else
       globulo.y = y_pos[2]
     end
-    print "---"
-    print (globulo.x)
-    print (globulo.y)
     counter = counter+1
     return globulo
   end
-  local group = self.view
   
-  a_long = create_globulo('long-a')
-  a_short = create_globulo('short-a')
   
-  e_long = create_globulo('long-e')
-  e_short = create_globulo('short-e')
+  a_long = create_globulo('long-a','a')
+  create_button_to_go(a_long, 'a')
+  
+  e_long = create_globulo('long-e','e')
+  create_button_to_go(e_long, 'e')
+  
+  i_long = create_globulo('long-i','i')  
+  create_button_to_go(i_long, 'i')
+  
+  o_long = create_globulo('long-o','o')
+  create_button_to_go(o_long, 'o')
+  
+  u_long = create_globulo('long-u','u')
+  create_button_to_go(u_long, 'u')
 
-  i_long = create_globulo('long-i')
-  i_short = create_globulo('short-i')
-
-  o_long = create_globulo('long-o')
-  o_short = create_globulo('short-o')
-
-  u_long = create_globulo('long-u')
-  u_short = create_globulo('short-u')
-
-
+  a_short = create_globulo('short-a','a')
+  e_short = create_globulo('short-e','e')
+  i_short = create_globulo('short-i','i')
+  o_short = create_globulo('short-o','o')
+  u_short = create_globulo('short-u','u')
+  
 
 end
 
