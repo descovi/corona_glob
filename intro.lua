028acc7af612a006f66838cb91265fb5603012e6
Simonini <descovi@gmail.com>
2012-09-30 10:34:52 +0200

    Semplificata la funzione che gestisce il click sul video.

--- intro.lua
+++ intro.lua
@@ -1,14 +1,15 @@
-
 local storyboard = require( "storyboard" )
 local intro = storyboard.newScene()
+
+function click_over_video( event )
+  storyboard.gotoScene("menu_iniziale")
+end
 
 function intro:createScene( event )
   local group = self.view
   local video = native.newVideo(0,0,1024,768)
   video:load("media/video.m4v")
-  local click_over_video = function( event )
-    storyboard.gotoScene("menu_iniziale")
-  end
+
   local bkgd = display.newRect( 0, 0, display.contentWidth, display.contentHeight )
   bkgd:setFillColor( 255, 0, 0 )
   bkgd:addEventListener("tap", click_over_video )
