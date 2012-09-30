7068ca24dc4105c03c8994e6205b79269e1a9715
Simonini <descovi@gmail.com>
2012-09-30 10:35:10 +0200

    Definizione di una variabile globale che tenga traccia delle scelte degli utenti.

--- main.lua
+++ main.lua
@@ -1,8 +1,12 @@
 local storyboard = require "storyboard"
 display.setStatusBar( display.HiddenStatusBar )
---Create a storyboard scene for this module
+
+_G.url = "nessuna"
+
+-- Create a storyboard scene for this module
 local scene = storyboard.newScene()
 storyboard.gotoScene( "menu_iniziale" )
 
 local gurgle = audio.loadSound('media/audio/GURGLE.wav')
-audio.play( gurgle ) 
+audio.play( gurgle )
+
