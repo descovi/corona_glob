4c11a00a12a022db6e0f455f4e4805bceee3ec52
Simonini <descovi@gmail.com>
2012-10-08 23:30:13 +0200

    La prima animazione con corona :)

--- colonna.lua
+++ colonna.lua
@@ -1,3 +1,5 @@
+require 'sprite'
+
 local storyboard = require( "storyboard" )
 local colonna = storyboard.newScene()
 local group
@@ -16,8 +18,13 @@ function colonna:createScene( event )
   local background = display.newImage("media/sfondi/".._G.vocale..".png")
   group:insert(background)
   -- anim
-  local anim = display.newImage(group, "media/colonna/a_e/1/full.png")
-  anim:addEventListener("tap", go_next_anim)
+  local sheet1 = graphics.newImageSheet("media/colonna/a_e/1/full.png",{ width=1024, height=256, numFrames=8 })
+  local instance1 = display.newSprite( sheet1, { name="cat", start=1, count=8, time=1000 } )
+  instance1:play()
+  instance1.x = 400
+  instance1.y = 400
+  group:insert(instance1)
+  --anim:addEventListener("tap", go_next_anim)
   -- torna indietro
   local torna_indietro = button_to_go_back()
   torna_indietro:addEventListener("tap", torna_indietro)
