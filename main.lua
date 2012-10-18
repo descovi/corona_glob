476de5f55bfb496e48f39d1ffb473f3f701d8164
Simonini <descovi@gmail.com>
2012-10-18 23:04:11 +0200

    Little patch to start the project with the new file system folder. NOW THE SIZE IS CORRECT!
    i dont know why but the size and dimenensions of elements is ok. before
    no. uao!

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+require ('src.scegli_combinazione')
 local storyboard = require "storyboard"
 display.setStatusBar( display.HiddenStatusBar )
 
@@ -6,7 +7,7 @@ _G.tipo = "lunga"
 
 -- Create a storyboard scene for this module
 local scene = storyboard.newScene()
-storyboard.gotoScene( "scegli_combinazione" )
+storyboard.gotoScene( "src.scegli_combinazione" )
 
 local gurgle = audio.loadSound('media/audio/GURGLE.wav')
 audio.play( gurgle )
