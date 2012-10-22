fe61ef5be7409e5bafa68c1fec04d41f4761fb16
Simonini <descovi@gmail.com>
2012-10-22 20:48:49 +0200

    scegli combinazione - Posizionato in maniera coretta gli elementi.

--- src/scegli_combinazione.lua
+++ src/scegli_combinazione.lua
@@ -1,6 +1,7 @@
 require('src.utils.button_to_go_back')
 
 local storyboard = require( "storyboard" )
+
 local scegli_combinazione = storyboard.newScene()
 
 function goto_confronto( event )
@@ -20,6 +21,7 @@ function scegli_combinazione:createScene( event )
   local group           = self.view
   local group_sopra     = display.newGroup()
   local group_sotto     = display.newGroup()
+
   group:insert(group_sopra)
   group:insert(group_sotto)
   local white_circle_1  = display.newImage( group_sopra, "media/sfondi/white_circle.png")
@@ -40,28 +42,29 @@ function scegli_combinazione:createScene( event )
   local _size           = 160
   local _font           = "Arial"
 
-  local y1              = 310
-  local y2              = display.contentHeight / 2
+  local y1              = white_circle_1.y -white_circle_1.height/2
+  local y2              = white_circle_2.y -white_circle_1.height/2
 
-  local x1              = 40
-  local x_plus          = 150
-  local x2              = display.contentWidth / 2
-  local x_equal         = 500
-  local x3              = 630
-  
+  local x1              = white_circle_1.x -300
+  local x_plus          = white_circle_1.x -200
+  local x2              = white_circle_1.x
+  local x_equal         = white_circle_1.x +100
+  local x3              = white_circle_1.x +200
+
+
   -- riga 1
-  local lettera_sx_1    = display.newText(group_sopra, _lettera_sx_0, x1,      y1, _font, _size)
+  local lettera_sx_1    = display.newText(group_sopra, _lettera_sx_0, x1,      y1-10, _font, _size)
   local plus            = display.newText(group_sopra, _plus,         x_plus,  y1, _font, _size)
-  local lettera_ml_1    = display.newText(group_sopra, _lettera_ml_1, x2,      y1, _font, _size)
+  local lettera_ml_1    = display.newText(group_sopra, _lettera_ml_1, x2-20,      y1, _font, _size)
   local equal           = display.newText(group_sopra, _equal,        x_equal, y1, _font, _size)
-  local lettera_dx_1    = display.newText(group_sopra, _lettera_dx_1, x3,      y1, _font, _size)
+  local lettera_dx_1    = display.newText(group_sopra, _lettera_dx_1, x3,      y1-10, _font, _size)
 
   -- riga 2
-  local lettera_sx_2    = display.newText(group_sotto, _lettera_sx_0, x1,      y2, _font, _size)
+  local lettera_sx_2    = display.newText(group_sotto, _lettera_sx_0, x1,      y2-10, _font, _size)
   local plus            = display.newText(group_sotto, _plus,         x_plus,  y2, _font, _size)
-  local lettera_ml_2    = display.newText(group_sotto, _lettera_ml_2, x2,      y2, _font, _size)
+  local lettera_ml_2    = display.newText(group_sotto, _lettera_ml_2, x2-40,      y2, _font, _size)
   local equal           = display.newText(group_sotto, _equal,        x_equal, y2, _font, _size)
-  local lettera_dx_2    = display.newText(group_sotto, _lettera_dx_2, x3,      y2, _font, _size)
+  local lettera_dx_2    = display.newText(group_sotto, _lettera_dx_2, x3,      y2-19, _font, _size)
   
   --- riga 1
   lettera_sx_1:setTextColor(200, 0, 0, 255)
