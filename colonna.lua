c0eb965fd8e03ada0492714144c6527c1e339f40
Simonini <descovi@gmail.com>
2012-10-23 21:36:25 +0200

    Definito il comportamento di introduzione.

--- colonna.lua
+++ colonna.lua
@@ -16,31 +16,34 @@ function generate_sprite()
   local _totalFrames = 11
   local sheet1_options = { width=1024, height=256, numFrames=_totalFrames }
   local sheet1 = graphics.newImageSheet("media/colonna/a_e/1/full.png", sheet1_options)
-  print (sheet1_options)
   local sequence_data = {
     { name="forward", start=1, count=_totalFrames, time=1000,  loopCount = 1}, 
     { name="bounce",  start=1, count=_totalFrames, time=1000,  loopCount = 1, loopDirection = "bounce" },
     { name="counter", frames= {11,10,9,8,7,6,5,4,3,2,1}, time=1000,  loopCount = 1}
   }
-  local instance1 = display.newSprite( sheet1, sequence_data )
-  instance1:setSequence( "counter" )
-  instance1:play()
-  instance1.x = display.contentWidth / 2
-  instance1.y = display.contentHeight / 2
-  anim_container:insert(instance1)
-  prima = true
-  local function spriteListener( event )
+  local anim = display.newSprite( sheet1, sequence_data )
+  anim:setSequence( "forward" )
+  anim:play()
+  anim.x = display.contentWidth / 2
+  anim.y = display.contentHeight / 2
+  anim_container:insert(anim)
+
+  local function intro( event )
     if event.phase == "began" then
       local gurgle = audio.loadSound('media/audio/a-e/fadfade/1.mp3')
       audio.play( gurgle )
     elseif event.phase == "ended" then
       local gurgle = audio.loadSound('media/audio/a-e/fadfade/2.mp3')
+      
       audio.play( gurgle )
-
+      local thisSprite = event.target
+      thisSprite:setSequence( "counter" )
+      thisSprite:play()
+      anim:removeEventListener( "sprite", intro )
     end
   end
   -- Add sprite listener
-  instance1:addEventListener( "sprite", spriteListener )
+  anim:addEventListener( "sprite", intro )
 
 end
 
