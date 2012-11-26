1f61f77fcbb26d965056d05aeb722a81a5fe0fad
Simonini <descovi@gmail.com>
2012-11-26 22:48:35 +0100

    Momentaneamente messo colonna come prima schermata.

--- main.lua
+++ main.lua
@@ -2,7 +2,6 @@ require ('src.scegli_combinazione')
 require ('src.confronto')
 local storyboard = require "storyboard"
 display.setStatusBar( display.HiddenStatusBar )
-
 _G.vocale = "a"
 _G.tipo = "lunga"
 _G.combinazione = "a_e"
@@ -10,7 +9,7 @@ _G.combinazione = "a_e"
 -- Create a storyboard scene for this module
 local scene = storyboard.newScene()
 storyboard.isDebug = true
-storyboard.gotoScene( "src.scegli_combinazione" )
+storyboard.gotoScene( "src.colonna" )
 
 local gurgle = audio.loadSound('media/audio/GURGLE.wav')
 --audio.play( gurgle )
