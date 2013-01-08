0d2ee368da8cd557554185a4a49417cac4ca25b3
Simonini <descovi@gmail.com>
2013-01-08 23:56:14 +0100

    Gioco funzionante a livello di meccaniche b ase

--- src/game.lua
+++ src/game.lua
@@ -5,25 +5,57 @@ local vocali = {"a","e","i","o","u"}
 
 local x_pos = {0, 100, 200, 300, 400}
 local y_pos = {550,100}
+local all_globuli = {}
 local path = 'media/menu_iniziale/'
+local punteggio
+local globulo_scelto
+local score = 0
+local punteggio = display.newText("0", 20,20,"Hiragino Maru Gothic Pro",40)
+
   -- Loading sound
 
 function pulsantone_clicked()
   print "pulsantone clicked!!"
+  globulo_scelto = all_globuli[ math.random(#all_globuli) ]
+  --audio.play(a.path_audio)
+  audio.play(globulo_scelto.audio)
+end
+function answer_clicked(event)
+  if globulo_scelto == event.target then
+    print "giusto!"
+    score = score+1
+    punteggio.text = score
+  else
+    print "cacca!"
+    score = score-1
+    punteggio.text = score
+  end
+  print(event.target)
 end
 function crea_fila(long_or_short)
   local group = display.newGroup()
   for i=1,5 do
     single_path = path .. long_or_short ..vocali[i] .. "-150/1.png"
-    a = display.newImage(single_path)
-    a.x = x_pos[i]*1.5 + a.width
-    group:insert(a)
+    local globo = display.newImage(single_path)
+    globo.x = x_pos[i]*1.5 + globo.width
+    -- sound
+    local path_audio = 'media/audio/vocali/'
+    if (long_or_short=="long-") then
+      local audio = audio.loadSound( path_audio .. string.upper(vocali[i]) .. '_L.mp3')
+      globo.audio = audio
+    else
+      local audio = audio.loadSound( path_audio .. string.upper(vocali[i]) .. '_S.mp3')
+      globo.audio = audio
+    end
+    -- tap
+    globo:addEventListener("tap", answer_clicked)
+    -- insert
+    table.insert(all_globuli,globo)
+    group:insert(globo)
   end
   return group
 end
-function crea_punteggio()
-  punteggio = display.newText("0", 0,0,"Hiragino Maru Gothic Pro",40)
-end
+
 function crea_pulsantone()
   local size_pulsantone = 400
   pulsantone = display.newImage("media/menu_iniziale/long-a/1.png")
@@ -38,10 +70,9 @@ function game:createScene(event)
 
   fila_short = crea_fila("short-")
   fila_long = crea_fila("long-")
-  fila_short.y = y_pos[1]
-  fila_long.y = y_pos[2]
+  fila_short.y = y_pos[2]
+  fila_long.y = y_pos[1]
 
-  crea_punteggio()
   crea_pulsantone()
   
 end
