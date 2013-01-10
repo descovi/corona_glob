05814354d0f1291563bccb973d6cf815ed9a36f3
Simonini <descovi@gmail.com>
2013-01-10 22:11:48 +0100

    button_to_go - parametro iniziale

--- src/utils/button_to_go.lua
+++ src/utils/button_to_go.lua
@@ -1,6 +1,7 @@
-function create_button_to_go(globulo, vocale)
+function create_button_to_go(globulo, vocale, y_pos)
+  y_pos = y_pos or 600
   local cerchio_container = display.newGroup()
-  local cerchio = display.newCircle(globulo.x, 600, 30)
+  local cerchio = display.newCircle(globulo.x, y_pos, 30)
   local testo = display.newText(vocale, cerchio.x-30, cerchio.y-14, "Hiragino Maru Gothic Pro", 100)
   testo:setTextColor(255, 255, 255)
   testo:setReferencePoint( display.CenterLeftReferencePoint )
