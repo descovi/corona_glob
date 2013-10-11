4b0074548324227c07dc1971002cb9872ca104ba
Simonini <descovi@gmail.com>
2013-10-11 19:14:57 +0200

    main is ok

--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
 inspect = require 'src.utils.inspect.inspect'
-
-require ('src.scegli_combinazione')
-require ('src.confronto')
-local storyboard = require "storyboard"
-display.setStatusBar( display.HiddenStatusBar )
 _G.vocale = "a"
 _G.tipo = "lunga"
 _G.combinazione = "a_e"
+
+local storyboard = require "storyboard"
+display.setStatusBar( display.HiddenStatusBar )
 
 -- Create a storyboard scene for this module
 local scene = storyboard.newScene()
