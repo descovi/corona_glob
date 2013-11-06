ccc295c65cf6640a30c9893e7bdf5f3a0ed180e3
Simonini <descovi@gmail.com>
2013-11-06 23:18:23 +0100

    play with intro

--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ display.setStatusBar( display.HiddenStatusBar )
 -- Create a storyboard scene for this module
 local scene = storyboard.newScene()
 storyboard.isDebug = true
-storyboard.gotoScene( "src.menu_iniziale" )
+storyboard.gotoScene( "src.intro" )
 
 local gurgle = audio.loadSound('media/audio/GURGLE.wav')
 audio.play( gurgle )
