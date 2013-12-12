19b2be8c0d8994dd2fa0d4bf96ceef86d9a25824
Alessandro <descovi@gmail.com>
2013-12-12 09:36:51 +0100

    nuovo pulsante per andare indietro

--- src/utils/button_to_go_back.lua
+++ src/utils/button_to_go_back.lua
@@ -1,7 +1,7 @@
 button_to_go_back = function ()
   local graphics  = display.newImage('media/torna_indietro.png')
   local size_wh   = 80
-  local margin    = 20
+  local margin    = 5
   graphics.x = display.contentWidth - (graphics.width/2) - margin
   graphics.y = size_wh
   if (nascondi) then
