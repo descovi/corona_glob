cdf3bd96ecdc79f48c163f6e51f198adcd59b9ee
Alessandro <descovi@gmail.com>
2013-12-12 12:15:58 +0100

    diminuita la dimensione del pulsante

--- src/utils/button_to_go_back.lua
+++ src/utils/button_to_go_back.lua
@@ -1,9 +1,10 @@
 button_to_go_back = function ()
   local graphics  = display.newImage('media/torna_indietro.png')
   local size_wh   = 80
-  local margin    = 5
-  graphics.x = display.contentWidth - (graphics.width/2) - margin
-  graphics.y = size_wh
+  local margin    = 10
+  graphics.x = display.contentWidth - (graphics.width/4) - margin
+  graphics.y = 60
+  graphics:scale(.5,.5)
   if (nascondi) then
     graphics.alpha = 0
   end
