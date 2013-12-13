37cb55c9f9074d5fdb7e63733cc0537376bc98d4
Alessandro <descovi@gmail.com>
2013-12-13 16:56:17 +0100

    main tolto delle procherie

--- main.lua
+++ main.lua
@@ -1,6 +1,5 @@
 require('src.utils.button_to_go_back')
 
-inspect = require 'src.utils.inspect.inspect'
 _G.vocale = "a"
 _G.tipo = "lunga"
 _G.combinazione = "a_e"
@@ -12,24 +11,8 @@ display.setStatusBar( display.HiddenStatusBar )
 
 -- Create a storyboard scene for this module
 local scene = storyboard.newScene()
-storyboard.isDebug = true
+storyboard.isDebug = false
 storyboard.gotoScene( "src.menu_iniziale" )
 
 local gurgle = audio.loadSound('media/audio/GURGLE.wav')
 --audio.play( gurgle )
-
-
-
-
-
-
-
-
-
--- Code to have Corona display the font names found
-local fonts = native.getFontNames()
-
-for k, v in ipairs(fonts) do
-     print(k, v)
-end
----------------------------------------------------------
