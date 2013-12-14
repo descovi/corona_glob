41e6677e70c12da7a09a36946004c36c3e8afe92
Alessandro <descovi@gmail.com>
2013-12-14 15:43:33 +0100

    confronto la lettera ora si muove due volte

--- src/confronto/letter.lua
+++ src/confronto/letter.lua
@@ -4,17 +4,29 @@ Letter.new = function(vocale)
 
   local short = display.newText(
     vocale,
-    display.contentWidth/2-10,
-    display.contentHeight-150,
-    _G.font,
-    60
+    display.contentWidth/2-20,
+    display.contentHeight-160,
+    _G.font_bold,
+    70
   )
 
-  short.zoom = function ( self )
+  short.zoom = function ( self, _delay )
     local original_size = self.size
-    local timing = 300
-    transition.to(self, { time=100,size=original_size*2 })  
-    transition.to(self, { time=100,delay=timing,size=original_size}) 
+    local timing = 200
+    -- first time
+    transition.to(self, { time=timing,size=original_size*2 })  
+    transition.to(self, { time=timing,delay=timing,size=original_size}) 
+    -- second time
+    transition.to(self, { 
+      time=timing,
+      size=original_size*2,
+      delay=_delay
+    })  
+    transition.to(self, { 
+      time=timing,
+      size=original_size,
+      delay=_delay+timing,
+    }) 
   end
 
   
