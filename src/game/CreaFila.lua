5b2b76d89b698f0e00643da717411fb2d094ba4f
Alessandro <descovi@gmail.com>
2013-12-19 11:38:39 +0100

    game - creafila - utilizzo corretto della variabile all_globuli

--- src/game/CreaFila.lua
+++ src/game/CreaFila.lua
@@ -1,6 +1,5 @@
-function CreaFila(long_or_short, vocali, path, x_pos, all_globuli)
+function CreaFila(long_or_short, vocali, path, x_pos)
   local group = display.newGroup()
-  
   for i=1, #vocali do
     local single_path = path .. long_or_short ..vocali[i] .. "-150/1.png"
     local globo = display.newImage(single_path)
@@ -8,27 +7,22 @@ function CreaFila(long_or_short, vocali, path, x_pos, all_globuli)
     globo.vocale = long_or_short..vocali[i]
     -- label
     
-
-    
     -- sound
     local path_audio = 'media/audio/vocali/'
     if (long_or_short=="long-") then
       local audio = audio.loadSound( path_audio .. string.upper(vocali[i]) .. '_L.mp3')
       globo.audio = audio
-    
     else
       local audio = audio.loadSound( path_audio .. string.upper(vocali[i]) .. '_S.mp3')
       globo.audio = audio
-      
     end
-
 
     -- tap
     globo:addEventListener("tap", answer_clicked)
+    globo.name="glob"
     -- insert
-    table.insert(all_globuli,globo)
     group:insert(globo)
-
+    
     if long_or_short == "short-" then
     local label = display.newText(vocali[i], 100,480,_G.font_bold,30)
       label.x = globo.x
