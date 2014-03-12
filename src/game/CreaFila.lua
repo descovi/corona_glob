dcf349d9db193d3f69e90ea2018927c1527e8581
Alessandro <descovi@gmail.com>
2014-03-12 18:10:23 +0100

    riposizionato un altro elemento nel gioco

--- src/game/CreaFila.lua
+++ src/game/CreaFila.lua
@@ -6,7 +6,7 @@ function CreaFila(long_or_short, vocali, path, x_pos)
     globo.x = x_pos[i]*1.5 + globo.width
     globo.vocale = long_or_short..vocali[i]
     -- label
-    
+
     -- sound
     local path_audio = 'media/audio/vocali/'
     if (long_or_short=="long-") then
@@ -22,7 +22,7 @@ function CreaFila(long_or_short, vocali, path, x_pos)
     globo.name="glob"
     -- insert
     group:insert(globo)
-    
+
     if long_or_short == "short-" then
     local label = display.newText(vocali[i], 100,480,_G.font_bold,30)
       label.x = globo.x
@@ -31,11 +31,11 @@ function CreaFila(long_or_short, vocali, path, x_pos)
       group:insert(label)
     end
 
-    
+
   end -- END FOR
 
   -- short long
-  local label = display.newText(group, long_or_short:gsub("-",""), group.x+group.width+80,group.y+group.height/4,_G.font,20)
+  local label = display.newText(group, long_or_short:gsub("-",""), group.x+group.width+100,group.y,_G.font,20)
 
   return group
 end
