b1932cc6a753b044c369a12194aabc31bdbd0787
Alessandro <descovi@gmail.com>
2014-04-09 01:15:43 +0200

    intro e video posizionati correttamente

--- src/intro.lua
+++ src/intro.lua
@@ -3,30 +3,32 @@ local intro = storyboard.newScene()
 
 function intro:createScene( event )
   local group = self.view
-  local video = native.newVideo(0,0,1024,768)
-  function click_over_video( event )
-  video:removeSelf()
-  storyboard.removeScene("src.intro")
-  storyboard.gotoScene("src.menu_iniziale")
-end
+  local video = native.newVideo(display.contentWidth/2,display.contentHeight/2,1024,768)
 
-local function videoListener( event )
-  print( "Event phase: " .. event.phase )
-  if event.phase == "ended" then
-    video:play()
+  local function click_over_video( event )
+    video:removeSelf()
+    storyboard.removeScene("src.intro")
+    storyboard.gotoScene("src.menu_iniziale")
   end
-  if event.errorCode then
-    native.showAlert( "Error!", event.errorMessage, { "OK" } )
+
+  local function videoListener( event )
+    print( "Event phase: " .. event.phase )
+    if event.phase == "ended" then
+      video:play()
+    end
+    if event.errorCode then
+      native.showAlert( "Error!", event.errorMessage, { "OK" } )
+    end
   end
-end
+
   video:load("media/video.m4v")
   video:play()
   video:addEventListener( "video", videoListener )
 
-  local bkgd = display.newRect( 0, 0, display.contentWidth, display.contentHeight )
-  bkgd:setFillColor( 0, 0, 0 )
+  local bkgd = display.newRect(display.contentWidth/2, display.contentHeight/2, display.contentWidth, display.contentHeight )
+  bkgd:setFillColor( 0, 0, 0, 0.5 )
   bkgd:addEventListener("tap", click_over_video )
-  video:addEventListener("tap", click_over_video )
+  --video:addEventListener("tap", click_over_video )
   group:insert(bkgd)
   group:insert(video)
 end
